af0a3da916ed30242583067c1faca212989122c3
Kazuya Numata <numata@antas.jp>
2018-11-13 13:04:57 +0900

    Apply style

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-import 'babel-polyfill'
-import Vue from 'vue'
+import 'babel-polyfill';
+import Vue from 'vue';
 
-import './plugins/vuetify'
-import App from './App.vue'
+import './plugins/vuetify';
+import Vuetify from 'vuetify';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify, {
+    theme: {
+        primary: '#ff0000',
+        secondary: '#b0bec5',
+        accent: '#8c9eff',
+        error: '#b71c1c'
+    }
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount('#app');
